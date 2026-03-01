9847f87226b90d592c95154c42a9c0982720c26c
mathisha034 <150885341+mathisha034@users.noreply.github.com>
2026-03-01 17:14:52 +0530

    spiker working

--- code/ip/02.encoding/spike_encoder/deterministic_rate_encoder/utils/spiker/tb_spiker/spiker_tb_v2.v
+++ code/ip/02.encoding/spike_encoder/deterministic_rate_encoder/utils/spiker/tb_spiker/spiker_tb_v2.v
@@ -3,13 +3,14 @@ module spiker_tb ();
 reg clock;
 reg [31:0] spike_window_time_input;
 wire spike_output;
+reg  spiker_enable_reg;
 // Global synapse time counter instance
 
     // Spiker module instance
 spiker_module spiker_module_instance_01 (
     .clock(clock),
     .spike_window_time_input(spike_window_time_input),
-    .spiker_enable_input(1'b1), // Enable the spiker to generate the spike output
+    .spiker_enable_input(spiker_enable_reg), // Enable the spiker to generate the spike output
     .spike_output(spike_output)
 );
 
@@ -23,16 +24,22 @@ initial begin
     $dumpvars(0, spiker_tb);
 
     // Initialize the clock signal
-    clock = 0;
+    clock = 1;
     // Clock toggles in separate always block
 
     // Test case 1: Set spike window time to 10 and observe the spike output
     #10; // Wait for some time before applying the test case
+    spiker_enable_reg = 1'b1; // Enable the spiker
     spike_window_time_input = 32'h0000000A; // Set spike window time to 10
-    #100; // Wait for some time to observe the spike output
+    #20; // Wait for the clock to toggle and the spiker to process the input
+    spiker_enable_reg = 1'b0; // Disable the spiker after processing
+    #2; // Wait for some time to observe the spike output
 
     // Test case 2: Set spike window time to 2 and observe the spike output
+    spiker_enable_reg = 1'b1; // Enable the spiker
     spike_window_time_input = 32'h00000002; // Set spike window time to 2
+    #4; // Wait for the clock to toggle and the spiker to process the input
+    spiker_enable_reg = 1'b0; // Disable the spiker after processing
     #100; // Wait for some time to observe the spike output
 
     // Test case 3: Set spike window time to 0 and observe the spike output
