e9250fe6fdbc78fe6944e85094e6fed03ff5c382
mathisha034 <150885341+mathisha034@users.noreply.github.com>
2026-03-12 18:11:51 +0530

    Remove unnecessary blank line in 8-bit to 32-bit extender module

--- code/ip/01.neurons/lif_standard_weighted/utils/8bit_to_32bit_extender/8bit_to32bit_extender.v
+++ code/ip/01.neurons/lif_standard_weighted/utils/8bit_to_32bit_extender/8bit_to32bit_extender.v
@@ -5,7 +5,6 @@ module bit_extender_8_to_32 (
 
 always @(input_8_bit) begin
     //extendoing the signed bit of 8 bit input to the upper 24 bits of the output
-
     output_wire <= {{24{input_8_bit[7]}}, input_8_bit}; // Concatenate 24 bits of the sign bit with the 8-bit input to create a 32-bit output    
 end
 endmodule
